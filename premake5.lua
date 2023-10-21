9f3eb0909bef228e66fedae7b459e9a1fbc6c21b
yinghaidada <pyj2897022134@gmail.com>
2023-10-21 21:42:54 +0800

    更新premake5

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 project "ImGui"
-    kind "StaticLib"
+	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	cppdialect "C++17"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,12 +24,10 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
 
 	filter "configurations:Debug"
 		runtime "Debug"
@@ -38,7 +37,7 @@ project "ImGui"
 		runtime "Release"
 		optimize "on"
 
-    filter "configurations:Dist"
+	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
-    symbols "off"
+		symbols "off"
